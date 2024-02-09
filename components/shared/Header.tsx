c765029004950d71537a4d40cf2b11e6931e57be
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2024-02-09 15:43:37 -0800

    Clerk installed for user auth, Custom SignedIn SignedOut UserButton imported from Clerk, middlerware.ts created for routes and ignored paths, .env.local created for secret api keys

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,8 +1,46 @@
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import { Button } from '../ui/button'
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <header className='w-full border-b'>
+            <div className='wrapper flex items-center justify-between'>
+                <Link
+                    href='/'
+                    className='w-36'
+                >
+                    <Image 
+                        src='/favicon.ico'
+                        width={100}
+                        height={100}
+                        alt='Spring 2 Action Logo'
+                    />
+                </Link>
+                <div className='flex w-32 justify-end gap-3'>
+                    <SignedIn>
+                        <UserButton 
+                            afterSignOutUrl='/'
+                        />
+                    </SignedIn>
+                    <SignedOut>
+                        <Button 
+                            asChild
+                            className='rounded-full'
+                            size='lg'
+                        >
+                            <Link
+                                href='/sign-in'
+                            >
+                                Login
+                            </Link>
+                        </Button>    
+                    </SignedOut>
+                </div>
+            </div>
+        </header>
     )
 }
 
