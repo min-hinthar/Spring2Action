08f8a4696a7c73bf5b4b1051b2b62cb20a426f10
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2024-02-09 16:19:17 -0800

    Footer.tsx updated with logo, Link and p text

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,8 +1,25 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer className='border-t'>
+            <div className='flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row'>
+                <Link href='/'>
+                    <Image 
+                        src='/favicon.ico'
+                        alt='logo'
+                        width={100}
+                        height={100}
+                    />
+                </Link>
+
+                <p>
+                    © 2024 | Coded with 💖 for Burma Spring Revolution ✊🏽
+                </p>
+            </div>
+        </footer>
     )
 }
 
