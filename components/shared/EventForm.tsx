cb654d5ff5d17fe7b4c8b461fde4f7433ba84a63
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2024-02-10 21:50:37 -0800

    Price input with Free Ticket checkbox and URL link to Create Event implemented

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -17,6 +17,7 @@ import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 
 import "react-datepicker/dist/react-datepicker.css";
+import { Checkbox } from "../ui/checkbox"
 
 
 
@@ -211,7 +212,90 @@ return (
                 />
             </div>
 
-                <Button type="submit">Submit</Button>
+            <div className="flex flex-col gap-5 md:flex-row">
+            {/* PRICE Form Field */}
+                <FormField
+                    control={form.control}
+                    name="price"
+                    render={({ field }) => (
+                    <FormItem className="w-full">
+                        <FormControl>
+                            <div className="flex-center h-[55px] w-full overflow-hidden rounded-full bg-gray-50 px-4 py-2">
+                                <Image 
+                                    src='/assets/icons/dollar.svg'
+                                    alt="dollar"
+                                    width={24}
+                                    height={24}
+                                />
+                                <Input 
+                                    type="number"
+                                    placeholder="Price"
+                                    {...field}
+                                    className="p-regular-16 border-0 bg-grey-50 outline-offset-0 focus:border-0 focus-visible:ring-0 focus-visible:ring-offset-0"
+                                />
+                                {/* FREE TICKET? Form Field */}
+                                <FormField
+                                    control={form.control}
+                                    name="isFree"
+                                    render={({ field }) => (
+                                    <FormItem>
+                                        <FormControl>
+                                            <div className="flex items-center">
+                                                <label 
+                                                    htmlFor="isFree"
+                                                    className="whitespace-nowrap pr-3 leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                                                >
+                                                    Free Ticket
+                                                </label>
+                                                <Checkbox id="isFree" className="mr-2 h-5 w-5 border-2 border-primary-500"/>
+                                            </div>
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                                />
+                            </div>
+                        </FormControl>
+                        <FormMessage />
+                    </FormItem>
+                )}
+                />
+                {/* URL Form Field */}
+                <FormField
+                    control={form.control}
+                    name="url"
+                    render={({ field }) => (
+                    <FormItem className="w-full">
+                        <FormControl>
+                            <div className="flex-center h-[55px] w-full overflow-hidden rounded-full bg-gray-50 px-4 py-2">
+                                <Image 
+                                    src='/assets/icons/link.svg'
+                                    alt="link"
+                                    width={24}
+                                    height={24}
+                                />
+                            <Input 
+                                placeholder="URL" {...field} 
+                                className="input-field"/>
+                            </div>
+                        </FormControl>
+                        <FormMessage />
+                    </FormItem>
+                )}
+                />
+                
+            </div>
+
+                <Button 
+                    type="submit"
+                    size='lg'
+                    disabled={form.formState.isSubmitting}
+                    className="button col-span-2 w-full"
+                >
+                    {form.formState.isSubmitting ? (
+                        'Submitting...'
+                    ) : `${type} Event`}
+                </Button>
         </form>
     </Form>
 )
