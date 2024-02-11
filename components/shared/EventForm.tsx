a75064a97bfc181a7207cf363b51af12aa98ba86
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2024-02-10 17:11:33 -0800

    EventForm updated with FileUploader component with uploadthing api FileRouter and createRouteHandler

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -3,13 +3,17 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { Button } from "@/components/ui/button"
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage,
+import { Form, FormControl, FormField, FormItem, FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { eventFormSchema } from "@/lib/validator"
 import * as z from "zod"
 import { eventDefaultValues } from "@/constants"
 import DropDown from "./DropDown"
+import { Textarea } from "@/components/ui/textarea"
+import { FileUploader } from "./FileUploader"
+import { useState } from "react"
+
 
 
 type EventFormProps = {
@@ -20,6 +24,7 @@ type EventFormProps = {
 const EventForm = ({ userId, type }: EventFormProps) => {
 
     const initialValues = eventDefaultValues;
+    const [files, setFiles] = useState<File[]>([]);
 
     // 1. Define your form.
 const form = useForm<z.infer<typeof eventFormSchema>>({
@@ -74,6 +79,40 @@ return (
                 />
 
             </div>
+
+            <div className="flex flex-col gap-5 md:flex-row">
+                <FormField
+                        control={form.control}
+                        name="description"
+                        render={({ field }) => (
+                        <FormItem className="w-full">
+                            <FormControl className="h-72">
+                                <Textarea 
+                                    placeholder="Description" {...field} 
+                                    className="textarea rounded-2xl"/>
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="imageUrl"
+                    render={({ field }) => (
+                    <FormItem className="w-full">
+                        <FormControl className="h-72">
+                            <FileUploader 
+                                onFieldChange={field.onChange}
+                                imageUrl={field.value}
+                                setFiles={setFiles}
+                            />
+                        </FormControl>
+                        <FormMessage />
+                    </FormItem>
+                )}
+                />
+            </div>
+
                 <Button type="submit">Submit</Button>
         </form>
     </Form>
